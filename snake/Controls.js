bc13207b9562341dd7ab85530afbd4a118e22d09
Jason Tucker <jtgeek@gmail.com>
2017-07-18 15:39:49 -0400

    Added wasd controls

--- snake/Controls.js
+++ snake/Controls.js
@@ -1,8 +1,14 @@
 const Direction = require('./Direction')
 
 module.exports.Keys = {
-        37: Direction.LEFT,
-        38: Direction.UP,
-        39: Direction.RIGHT,
-        40: Direction.DOWN
-    }
+    //wasd
+    65: Direction.LEFT,
+    87: Direction.UP,
+    68: Direction.RIGHT,
+    83: Direction.DOWN,
+    //arrows
+    37: Direction.LEFT,
+    38: Direction.UP,
+    39: Direction.RIGHT,
+    40: Direction.DOWN
+}
